90bd1f67d71fe117d456321474fcfdfef2362c8c
petersonuche <petersonucche@gmail.com>
2022-10-18 03:21:22 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints digits from base 10 starting 0
+ *
+ * Return: 0
+ */
+int maim(void)
+{
+	int a;
+	
+	a = 0
+	for (a < 10)
+	{
+		putchar(c + '0');
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
