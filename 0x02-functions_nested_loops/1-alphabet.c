e7c733aacfbdce318b941c6bf23b01805dac65be
petersonuche <petersonucche@gmail.com>
2022-10-18 17:03:52 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the letters in lowercas
+ *
+ */
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
+
+
